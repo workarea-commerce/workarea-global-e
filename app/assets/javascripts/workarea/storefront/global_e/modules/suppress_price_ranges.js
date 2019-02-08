7da795316ca319676c34150fd863bdb01b4b06c4
Eric Pigeon <epigeon@weblinc.com>
2019-02-08 10:45:58 -0500

    Supress Price Filters For GE Browsing
    Hide price filters when GlobalE is doing price conversion.
    
    no changelog
    
    GLOBALE-10

---
+++ app/assets/javascripts/workarea/storefront/global_e/modules/suppress_price_ranges.js
@@ -0,0 +1,30 @@
+/**
+ * @namespace WORKAREA.global_e
+ */
+
+WORKAREA.registerModule('global_e_price_filter', (function () {
+    var globalECookie = function() {
+            try {
+                return JSON.parse(decodeURIComponent(WORKAREA.cookie.read('GlobalE_Data'))) || {};
+            }
+            catch (err) {
+                return {};
+            }
+        },
+
+        shippingCountry = function() {
+            return globalECookie().countryISO;
+        };
+
+        init = function($scope) {
+            if (_.isNull(shippingCountry()) || _.includes(WORKAREA.config.globalE.domesticCountries, shippingCountry())) {
+                return;
+            }
+
+            $('.result-filters__section--price', $scope).hide();
+        };
+
+    return {
+        init: init
+    };
+}()));
--- config/initializers/appends.rb
+++ config/initializers/appends.rb
@@ -7,3 +7,13 @@ Workarea.append_partials(
   'storefront.footer_help',
   'workarea/storefront/global_e/country_picker'
 )
+
+Workarea.append_javascripts(
+  "storefront.config",
+  "workarea/storefront/global_e/global_e_config"
+)
+
+Workarea.append_javascripts(
+  "storefront.modules",
+  "workarea/storefront/global_e/modules/suppress_price_ranges"
+)
