3bf292ed267e5df358f92615477825b9f766d0a3
Eric Pigeon <epigeon@weblinc.com>
2019-03-08 14:51:24 -0500

    Clear Order#user_id before entering GlobalE checkout as a guest
    no changelog
    
    GLOBALE-19

--- test/integration/workarea/storefront/global_e_checkouts_integration_test.rb
+++ test/integration/workarea/storefront/global_e_checkouts_integration_test.rb
@@ -40,6 +40,39 @@ module Workarea
         get storefront.checkout_path
         assert_redirected_to storefront.ge_checkout_url
       end
+
+      def test_guest_checkout_clears_user_order_details
+        cookies['GlobalE_Data'] = JSON.generate({
+          "countryISO" => "CA",
+          "currencyCode" => "CAD",
+          "cultureCode" => "en-CA"
+        })
+
+        user = create_user(password: 'W3bl1nc!')
+
+        post storefront.login_path,
+          params: { email: user.email, password: 'W3bl1nc!' }
+
+        product = create_product
+
+        post storefront.cart_items_path,
+          params: {
+            product_id: product.id,
+            sku: product.skus.first,
+            quantity: 2
+          }
+
+        order = Workarea::Order.first
+        assert_equal user.id.to_s, order.user_id
+
+        cookies.delete "user_id"
+
+        get storefront.checkout_path
+        assert_redirected_to storefront.ge_checkout_url
+
+        order.reload
+        assert_nil order.user_id
+      end
     end
   end
 end
